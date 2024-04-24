4a80be1d5743a8d3db06d27893e970125e28300c
IgnacioGDV <166122043+IgnacioGDV@users.noreply.github.com>
2024-04-24 12:18:57 +0200

    Crear clase Tarjeta.cs

---
+++ MaquinaVending/Tarjeta.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MáquinaDeVending
+{
+    internal class Tarjeta
+    {
+        public string Número { get; set; }
+        public string FechaCaducidad { get; set; }
+        public string CódigoSeguridad { get; set; }
+        public bool ArgumentoNoValido { get; set; }
+        public Tarjeta()
+        {
+            this.ArgumentoNoValido = false;
+        }
+    }
+}
